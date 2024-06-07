85ab5997a69905e3033b96816e4a817b35fc003c
Athiqur01 <athik.apece@gmail.com>
2024-06-07 23:45:32 +0600

    token verify

--- index.js
+++ index.js
@@ -64,6 +64,23 @@ async function run() {
         res.send(result)
     })
 
+    //verify token------
+    const verifyToken=(req,res,next)=>{
+      console.log('inside verify token', req.headers.authorization)
+      if(!req.headers.authorization){
+        return res.status(401).send({message:'forbidden access'})
+      }
+      const token=req.headers.authorization.split(' ')[1]
+      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+          return res.status(401).send({message:'forbidden access'})
+        }
+        req.decoded=decoded
+        next()
+
+      })
+    }
+
     app.post('/jwt', async(req,res)=>{
       const user=req.body
       console.log(user)
@@ -93,8 +110,8 @@ async function run() {
         res.send(user)
     })
 
-    app.get("/allUsers", async(req,res)=>{
-      console.log('inside verify token', req.headers.authorization)
+    app.get("/allUsers",verifyToken, async(req,res)=>{
+     // console.log('inside verify token', req.headers.authorization)
         const cursor=await userCollection.find()
         const user=await cursor.toArray(cursor)
         res.send(user)
